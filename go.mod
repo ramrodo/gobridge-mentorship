0b85380894fd077263d001a6dea12f8dfdb20314
Rodolfo Martínez Vega <rodomar@outlook.com>
2020-11-28 14:35:18 -0600

    Create, get and delete a key

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ramrodo/gobridge-mentorship
 
 go 1.15
 
-require github.com/dgraph-io/badger/v2 v2.2007.2 // indirect
+require github.com/dgraph-io/badger/v2 v2.2007.2
--- main.go
+++ main.go
@@ -1,18 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	badger "github.com/dgraph-io/badger/v2"
 )
 
 func main() {
-	// Open the Badger database located in the /tmp/badger directory.
-	// It will be created if it doesn't exist.
-	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	// Create/Open a temporal database
+	db, err := badger.Open(badger.DefaultOptions("/tmp/test1"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	// Your code here…
+
+	// Key
+	key := "user"
+
+	// Create/Update a key
+	err = db.Update(func(txn *badger.Txn) error {
+		err := txn.Set([]byte(key), []byte("ramrodo"))
+		return err
+	})
+
+	if err != nil {
+		log.Fatalf("Error creating/updating the key '%v': %s", key, err)
+	}
+
+	// Get the key
+	var valCopy []byte
+	err = db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+		err = item.Value(func(val []byte) error {
+			valCopy = append([]byte{}, val...)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Error getting the key '%v': %s", key, err)
+	}
+
+	fmt.Printf("The key '%v' has value '%s'\n", key, valCopy)
+
+	// Delete the key
+	err = db.Update(func(txn *badger.Txn) error {
+		err := txn.Delete([]byte(key))
+		return err
+	})
+
+	if err != nil {
+		log.Fatalf("Error deleting the key '%v': %s", key, err)
+	}
 }
