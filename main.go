da638d4f425fb50fceb9d867f2cf5ac79de57341
Rodolfo Martínez Vega <rodomar@outlook.com>
2020-12-09 16:42:57 -0600

    Delete badger implementation. It'll be replaced by using a simple map

--- main.go
+++ main.go
@@ -1,63 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	badger "github.com/dgraph-io/badger/v2"
-)
+import "fmt"
 
 func main() {
-	// Create/Open a temporal database
-	db, err := badger.Open(badger.DefaultOptions("/tmp/test1"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// Key
-	key := "user"
-
-	// Create/Update a key
-	err = db.Update(func(txn *badger.Txn) error {
-		err := txn.Set([]byte(key), []byte("ramrodo"))
-		return err
-	})
-
-	if err != nil {
-		log.Fatalf("Error creating/updating the key '%v': %s", key, err)
-	}
-
-	// Get the key
-	var valCopy []byte
-	err = db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte(key))
-		if err != nil {
-			return err
-		}
-		err = item.Value(func(val []byte) error {
-			valCopy = append([]byte{}, val...)
-			return nil
-		})
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Fatalf("Error getting the key '%v': %s", key, err)
-	}
-
-	fmt.Printf("The key '%v' has value '%s'\n", key, valCopy)
-
-	// Delete the key
-	err = db.Update(func(txn *badger.Txn) error {
-		err := txn.Delete([]byte(key))
-		return err
-	})
-
-	if err != nil {
-		log.Fatalf("Error deleting the key '%v': %s", key, err)
-	}
+	fmt.Println("Hello world!")
 }
